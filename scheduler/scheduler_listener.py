ecc6b42b6a5e96f7c4802d28a86f23e73f22fea7
xuexiang <578337729@qq.com>
2019-12-27 14:22:06 +0800

    [新增]事件执行

--- scheduler/scheduler_listener.py
+++ scheduler/scheduler_listener.py
@@ -4,25 +4,31 @@
 import re
 
 from configs import log
-from util.request_package import request
+from util.request_package import request, event_request
 
 
 def listener(event):
     """监听器"""
     # 获取执行id, 任务id
-    [(exec_id, interface_id, job_id)] = re.findall('exec_(\\d+)_(\\d+)_(\\d+)', event.job_id)
+    [(exec_type, exec_id, interface_id, job_id)] = re.findall('(\\w+)_(\\d+)_(\\d+)_(\\d+)', event.job_id)
     # 异常
     if event.exception:
         log.warn('执行完毕, 执行id: %s, 任务流id: %s, 任务id: %s, 任务状态: %s' % (exec_id, interface_id, job_id, 'failed'))
         # 回调web服务
-        result = request(exec_id, interface_id, job_id, 'failed')
+        if exec_type == 'exec':
+            result = request(exec_id, interface_id, job_id, 'failed')
+        else:
+            result = event_request(exec_id, interface_id, job_id, 'failed')
         if not result:
             log.error('回调web服务失败, 执行id: %s, 任务流id: %s, 任务id: %s, 任务状态: %s' % (exec_id, interface_id, job_id, 'failed'))
     # 正常
     else:
         log.info('执行完毕, 执行id: %s, 任务流id: %s, 任务id: %s, 任务状态: %s' % (exec_id, interface_id, job_id, 'succeeded'))
         # 回调web服务
-        result = request(exec_id, interface_id, job_id, 'succeeded')
+        if exec_type == 'exec':
+            result = request(exec_id, interface_id, job_id, 'succeeded')
+        else:
+            result = event_request(exec_id, interface_id, job_id, 'succeeded')
         if not result:
-            log.error('回调web服务失败, 执行id: %s, 任务流id: %s, 任务id: %s, 任务状态: %s' % (
-                exec_id, interface_id, job_id, 'succeeded'))
+            log.error(
+                '回调web服务失败, 执行id: %s, 任务流id: %s, 任务id: %s, 任务状态: %s' % (exec_id, interface_id, job_id, 'succeeded'))
--- util/request_package.py
+++ util/request_package.py
@@ -22,3 +22,21 @@ def request(exec_id, interface_id, job_id, status):
         except Exception as e:
             log.error('回调web服务失败: [params: %s][error: %s]' % (str(params), e), exc_info=True)
             return False
+
+
+def event_request(exec_id, interface_id, job_id, status):
+    """封装requests请求"""
+    url = 'http://%(host)s:%(port)s/event/callback/' % {'host': config.master.host, 'port': config.master.port}
+    params = {'exec_id': exec_id, 'interface_id': interface_id, 'job_id': job_id, 'status': status}
+
+    for _ in range(5):
+        try:
+            response = requests.get(url=url, params=params)
+            log.debug('回调web服务响应结果：[status_code: %d][text: %s]' % (response.status_code, response.text))
+            if response.status_code == 200:
+                return True
+            else:
+                return False
+        except Exception as e:
+            log.error('回调web服务失败: [params: %s][error: %s]' % (str(params), e), exc_info=True)
+            return False
